214ceba4851947e4c69ecb378e5275457def7f12
Samuel Soares Silva <e.samuel.csilva@mpt.mp.br>
2024-06-19 16:39:13 -0300

    Create base business rules

--- src/main/java/com/samuel/ssilva/fileHandler/imageFile/ImageFileService.kt
+++ src/main/java/com/samuel/ssilva/fileHandler/imageFile/ImageFileService.kt
@@ -1,5 +1,25 @@
 package com.samuel.ssilva.fileHandler.imageFile
 
-class ImageFileService {
+import jakarta.servlet.ServletContext
+import org.springframework.http.ResponseEntity
+import org.springframework.web.multipart.MultipartFile
+import java.io.File
 
+class ImageFileService(
+    private val servlet: ServletContext,
+) {
+
+    fun saveImage(image: MultipartFile): ResponseEntity<String>?{
+        if (image.isEmpty){
+            return ResponseEntity.badRequest().body("Error by receiving the image")
+        }
+        try {
+            val fileName:String = image.originalFilename;
+            val path: String = servlet.getRealPath("/")+ "resources/" + fileName;
+            image.transferTo(File(path))
+        }catch (e:Exception){
+            e.printStackTrace()
+        }
+        return null
+    }
 }
