51ac2438754bc3c75538e69945453ebcc74f19d5
Samuel Soares Silva <e.samuel.csilva@mpt.mp.br>
2024-07-02 15:36:01 -0300

    Implements equals and hashCode manually and implements builder class

--- src/main/java/com/samuel/ssilva/fileHandler/imageFile/ImageFile.kt
+++ src/main/java/com/samuel/ssilva/fileHandler/imageFile/ImageFile.kt
@@ -3,16 +3,54 @@ package com.samuel.ssilva.fileHandler.imageFile
 import jakarta.persistence.*
 
 @Entity(name = "IMAGE_FILE_TB")
-class ImageFile {
+data class ImageFile (
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE) private val id: Long? = null,
 
-    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private val id: Long? = null
+    private val name: String? = null,
+    private val type: String? = null,
 
-    private val name: String? = null
-    private val type: String? = null
+    @Column(length = 5000000) private val imgByte: ByteArray? = null,
 
-    @Column(length = 5000000)
-    private val imgByte: ByteArray? = null
 
-    constructor()
+){
+    class Builder {
+        private var id: Long? = null
+        private var name: String? = null
+        private var type: String? = null
+        private var imgByte: ByteArray? = null
+
+        fun id(id: Long?) = apply { this.id = id }
+        fun name(name: String?) = apply { this.name = name }
+        fun type(type: String?) = apply { this.type = type }
+        fun imgByte(imgByte: ByteArray?) = apply { this.imgByte = imgByte }
+
+        fun build() = ImageFile(id, name, type, imgByte)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ImageFile
+
+        if (id != other.id) return false
+        if (name != other.name) return false
+        if (type != other.type) return false
+        if (imgByte != null) {
+            if (other.imgByte == null) return false
+            if (!imgByte.contentEquals(other.imgByte)) return false
+        } else if (other.imgByte != null) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id?.hashCode() ?: 0
+        result = 31 * result + (name?.hashCode() ?: 0)
+        result = 31 * result + (type?.hashCode() ?: 0)
+        result = 31 * result + (imgByte?.contentHashCode() ?: 0)
+        return result
+    }
 }
+
+
