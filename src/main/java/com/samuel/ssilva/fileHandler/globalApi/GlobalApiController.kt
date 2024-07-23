dcfa7c76543ecfad283ceec49e2f639aa76f8b12
Samuel Soares Silva <e.samuel.csilva@mpt.mp.br>
2024-07-23 14:54:25 -0300

    Refactor GlobalApiController

--- src/main/java/com/samuel/ssilva/fileHandler/globalApi/GlobalApiController.kt
+++ src/main/java/com/samuel/ssilva/fileHandler/globalApi/GlobalApiController.kt
@@ -11,5 +11,15 @@ import org.springframework.web.bind.annotation.RestController
 class GlobalApiController {
 
     @GetMapping
-    fun isApiRunning(): ResponseEntity<String> = ResponseEntity("<h1>Api is running</h1><br/> <img src=\"https://i0.wp.com/motiongraphicsphoebe.wordpress.com/wp-content/uploads/2018/10/loading-animations-preloader-gifs-ui-ux-effects-18.gif?w=1000&h=&ssl=1\" alt=\"Running\">", HttpStatus.OK)
+    fun isApiRunning(): ResponseEntity<String> {
+        val gifUrl: String = "https://i0.wp.com/motiongraphicsphoebe.wordpress.com/wp-content/uploads/2018/10/loading-animations-preloader-gifs-ui-ux-effects-18.gif?w=1000&h=&ssl=1"
+        val html: String =
+                "<body style=\"padding: 0; margin: 0;\">\n" +
+                "    <div style=\"width: 100vw; height: 100vh; display: flex; align-items: center; justify-content: center; flex-direction: column;\">\n" +
+                "        <H2>Service is running</H1>\n" +
+                "        <img src=\"${gifUrl}\" alt=\"Running\">\n" +
+                "    </div>\n" +
+                "</body>"
+        return ResponseEntity(html, HttpStatus.OK)
+    }
 }
