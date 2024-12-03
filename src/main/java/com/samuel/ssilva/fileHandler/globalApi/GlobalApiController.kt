2d9ec35dc1a7a8be049c3eeaabf2dc296a5f0a63
Samuel Soares <samuel.soares.cunha64@gmail.com>
2024-12-03 16:11:26 -0300

    Not draggable html

--- src/main/java/com/samuel/ssilva/fileHandler/globalApi/GlobalApiController.kt
+++ src/main/java/com/samuel/ssilva/fileHandler/globalApi/GlobalApiController.kt
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.swing.text.html.HTML
 
 @RestController
 @RequestMapping("/api")
@@ -14,12 +15,27 @@ class GlobalApiController {
     fun isApiRunning(): ResponseEntity<String> {
         val gifUrl: String = "https://i0.wp.com/motiongraphicsphoebe.wordpress.com/wp-content/uploads/2018/10/loading-animations-preloader-gifs-ui-ux-effects-18.gif?w=1000&h=&ssl=1"
         val html: String =
-                "<body style=\"padding: 0; margin: 0;\">\n" +
-                "    <div style=\"width: 100vw; height: 100vh; display: flex; align-items: center; justify-content: center; flex-direction: column;\">\n" +
-                "        <H1>Service is running</H1>\n" +
-                "        <img src=\"${gifUrl}\" alt=\"Running\">\n" +
-                "    </div>\n" +
-                "</body>"
+                "<body style=\"padding: 0; margin: 0; user-drag: none; user-select: none;\n" +
+                        "  user-drag: none;\n" +
+                        "  user-select: none;\n" +
+                        "  -moz-user-select: none;\n" +
+                        "  -webkit-user-drag: none;\n" +
+                        "  -webkit-user-select: none;\n" +
+                        "  -ms-user-select: none;\n" +
+                        "  pointer-events: none;\">\n" +
+                        "    <div style=\"width: 100vw; height: 100vh; display: flex; align-items: center; justify-content: center; flex-direction: column; \">\n" +
+                        "       <H1>Service is running</H1>\n" +
+                        "       <img\n" +
+                        "               style=\"user-drag: none;\n" +
+                        "                      user-select: none;\n" +
+                        "                      -moz-user-select: none;\n" +
+                        "                      -webkit-user-drag: none;\n" +
+                        "                      -webkit-user-select: none;\n" +
+                        "                      -ms-user-select: none;\n" +
+                        "                      pointer-events: none;\"\n" +
+                        "               src=\"${gifUrl}\" alt=\"Running\">\n" +
+                        "   </div>\n" +
+                        "</body>"
         return ResponseEntity(html, HttpStatus.OK)
     }
 }
