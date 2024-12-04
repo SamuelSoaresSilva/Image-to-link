38f7c7992c9054ebbd7795117b78802b8cca2400
Samuel Soares <samuel.soares.cunha64@gmail.com>
2024-12-04 13:58:43 -0300

    Refactor controller methods

--- src/main/java/com/samuel/ssilva/fileHandler/imageFile/ImageFileController.kt
+++ src/main/java/com/samuel/ssilva/fileHandler/imageFile/ImageFileController.kt
@@ -1,7 +1,6 @@
 package com.samuel.ssilva.fileHandler.imageFile
 
 import jakarta.servlet.http.HttpServletRequest
-import jakarta.validation.Valid
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -16,10 +15,10 @@ private class ImageFileController(
 
     @PostMapping(consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
     @Throws(IOException::class)
-    fun uploadImage(@RequestParam image: MultipartFile): ResponseEntity<Any>? = service.saveValidatedImage(image)
+    fun uploadImage(@RequestParam image: MultipartFile): ResponseEntity<out Any> = service.saveValidatedImage(image)
 
     @GetMapping("/{imageName}")
-    fun getImage(@PathVariable imageName: String): ResponseEntity<Any>? = service.returnValidatedImage(imageName)
+    fun getImage(@PathVariable imageName: String): ResponseEntity<out Any> = service.returnValidatedImage(imageName)
 
     @GetMapping("/info/{imageName}")
     fun getImageInfo(@PathVariable imageName: String, request: HttpServletRequest): ResponseEntity<Any>? = service.returnImageInfo(imageName, request)
@@ -28,5 +27,5 @@ private class ImageFileController(
     fun getAllImages(): ResponseEntity<Any>? = service.returnAllImages()
 
     @DeleteMapping("/delete/{imageName}")
-    fun deleteImage(@PathVariable imageName: String): ResponseEntity<DeleteImageView> = service.deleteImage(imageName)
+    fun deleteImage(@PathVariable imageName: String): ResponseEntity<ImageNameResponse> = service.deleteImage(imageName)
 }
